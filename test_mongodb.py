28805dfaef5289aa4c082a2bf437a38b74bc5136
Vrushti Patel <vrushtip2006@gmail.com>
2026-09-25 19:02:29 +0530

    Connected to MongoDB successfully :)

---
+++ test_mongodb.py
@@ -0,0 +1,13 @@
+from pymongo.mongo_client import MongoClient
+
+uri = "mongodb+srv://Vrushti:vrushtipatel2006@cluster0.8qf4u8g.mongodb.net/?appName=Cluster0"
+
+# Create a new client and connect to the server
+client = MongoClient(uri)
+
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
